62dfb25d6c8c1065df7f37b30e9f65cbf70f2383
Mateus Galdino <42646579+GaldinoMateus@users.noreply.github.com>
2022-05-16 10:26:46 -0300

    first commit

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,12 @@
+// Console.log com a primeira letra maiuscula da error por não ser case senstive
+console.log("Trabalhando com variáveis");
+//JS é case sensitive
+
+
+const idade = 29;
+console.log(idade);
+
+
+
+// Não fazer 
+// mes = "Janeiro"
---
+++ 5-atribuicaoDeVariaveis.js
@@ -0,0 +1,14 @@
+console.log("Trabalhando com variaveis");
+const idade = 29;
+let nome = "Mateus"; //let é usada para variaveis que mudam ao decorrer do codigo
+const sobrenome = "Galdino";
+
+// console.log(nome + " " + sobrenome); Mesma coisa que o de baixo
+// console.log(nome, sobrenome); Mesma coisa que o de baixo
+console.log(`Meu nome é ${nome} ${sobrenome}`);
+
+nome = nome + sobrenome;
+
+console.log(`Meu nome é ${nome}`);
+
+
